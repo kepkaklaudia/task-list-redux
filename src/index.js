41e7355266aad3ecd122a92c15a7e61f9ca5bb36
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-04 22:33:22 +0100

    application split into features

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from './GlobalStyle';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+        <App />
     </ThemeProvider>
   </React.StrictMode>
 );
