20de30b4d50add7e6c5534dfa4127b64d1c497e7
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-05 17:06:22 +0100

    Provider added

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import { GlobalStyle } from './GlobalStyle';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
-import App from './App';
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <App />
-    </ThemeProvider>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
