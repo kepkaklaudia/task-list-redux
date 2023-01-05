ff007035e8451c7df485f353009781de99e5e146
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-05 18:30:08 +0100

    setAllDone reducer added

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,8 +1,8 @@
 import { Button, Container } from "./styled";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleHideDone } from "../tasksSlice";
+import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
 
-const Buttons = ({ setAllDone }) => {
+const Buttons = () => {
   const { tasks, hideDone } = useSelector(selectTasks);
   const dispatch = useDispatch();
   return (
@@ -15,7 +15,7 @@ const Buttons = ({ setAllDone }) => {
             {hideDone ? "Pokaż " : "Ukryj "} ukończone
           </Button>
           <Button
-            onClick={setAllDone}
+            onClick={() => dispatch(setAllDone())}
             disabled={tasks.every(({ done }) => done)}
           >
             Ukończ wszystkie
--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,30 +4,21 @@ import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import { Container } from "../../common/Container/styled";
-import { useTasks } from "../../useTasks";
 
 function Tasks() {
-
-  const {
-    setAllDone,
-  } = useTasks();
-
   return (
     <Container>
-      <Header title="Lista zadań" />
+      <Header
+        title="Lista zadań"
+      />
       <Section
         title="Dodaj nowe zadanie"
-        body={
-          <Form />
-        }
+        body={<Form />}
       />
-      <Section title="Lista zadań"
-        body={ <TaskList /> }
-        extraHeaderContent={
-          <Buttons
-            setAllDone={setAllDone}
-          />
-        }
+      <Section
+        title="Lista zadań"
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
