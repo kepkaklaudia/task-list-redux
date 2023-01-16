ed6aca78891372fbfc2cce4daf83e0600cb91eca
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-16 14:18:29 +0100

    UnorderedList added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
 import Author from "./features/tasks/author/Author";
-import { StyledNavLink, Navigation } from "./styled";
+import { StyledNavLink, Navigation, UnorderedList } from "./styled";
 
 const App = () => (
   <>
     <BrowserRouter>
       <Navigation>
-        <ul>
+        <UnorderedList>
           <li>
             <StyledNavLink to="/zadania">Zadania</StyledNavLink>
           </li>
           <li>
             <StyledNavLink to="/autor">O autorze</StyledNavLink>
           </li>
-        </ul>
+        </UnorderedList>
       </Navigation>
         <Switch>
           <Route path="/zadania">
--- src/styled.js
+++ src/styled.js
@@ -5,6 +5,10 @@ export const Navigation = styled.nav`
   background-color: ${({theme}) => theme.color.teal};
 `;
 
+export const UnorderedList = styled.ul`
+  list-style-type: none;
+`;
+
 export const StyledNavLink = styled(NavLink)`
   text-decoration: none;
   color: ${({theme}) => theme.color.white};
