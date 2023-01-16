15479cb04b59b78263b089b8aae6af99d6062958
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-16 10:59:02 +0100

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import Tasks from "./features/tasks/Tasks";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
-const App = () => <Tasks />;
-
-export default App;
+export default () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania">
+          <Tasks />
+        </Route>
+      </Switch>
+    </nav>
+  </BrowserRouter>
+);
