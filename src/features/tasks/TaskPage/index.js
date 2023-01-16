cf6735959cb601a8d3efe516ddc5beddb762f4fb
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-16 23:00:14 +0100

    Section title added

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,10 +1,13 @@
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { Container } from "../../../common/Container/styled";
 import Header from "../../../common/Header";
 import Section from "../../../common/Section";
+import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
   const { id } = useParams();
+  const task = useSelector(state => getTaskById(state, id));
 
   return (
     <Container>
@@ -12,7 +15,7 @@ function TaskPage() {
         title="Szczegóły zadania"
       />
       <Section
-        title={id}
+        title={task ? task.content : "😨 Nie znaleziono zadania"}
         body={<>details</>}
       />
     </Container>
