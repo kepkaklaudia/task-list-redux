46b769dbcc567737db31e20031f5f2a853bbdbb8
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-16 11:17:16 +0100

    Author.js content added

--- src/features/tasks/author/Author.js
+++ src/features/tasks/author/Author.js
@@ -1,3 +1,19 @@
-const Author = () => {};
+import { Container } from "../../common/Container/styled";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+const Author = () => (
+  <>
+    <Container>
+      <Header
+        title="O autorze"
+      />
+      <Section
+        title="Klaudia Kępka"
+        body={<>tresc</>}
+      />
+      </Container>
+  </>
+);
 
 export default Author;
