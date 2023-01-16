f49c685d15819c817372fba413c9f5a3e255c2c5
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-16 13:24:04 +0100

    StyledNavLink styled

--- src/styled.js
+++ src/styled.js
@@ -3,4 +3,9 @@ import { NavLink } from "react-router-dom";
 
 export const StyledNavLink = styled(NavLink)`
   text-decoration: none;
+  color: ${({theme}) => theme.color.white};
+
+  &.active {
+    font-weight: bold;
+  }
 `;
