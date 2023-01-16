200cf751f5c8ca79290656740f8a8a1747b3fbed
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-16 21:26:12 +0100

    TaskPage subpage added

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,5 +1,22 @@
-function TaskPage () {
+import { useParams } from "react-router-dom";
+import { Container } from "../../../common/Container/styled";
+import Header from "../../../common/Header";
+import Section from "../../../common/Section";
 
+function TaskPage() {
+  const params = useParams();
+
+  return (
+    <Container>
+      <Header
+        title="Szczegóły zadania"
+      />
+      <Section
+        title={params.id}
+        body={<>details</>}
+      />
+    </Container>
+  );
 };
 
 export default TaskPage;
