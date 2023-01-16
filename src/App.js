01ab9d4ff71e26f8a0635057933fe652f3de2272
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-16 21:04:25 +0100

    TaskPage added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
 import Author from "./features/tasks/author/Author";
 import { StyledNavLink, Navigation, UnorderedList } from "./styled";
 
@@ -17,14 +18,17 @@ const App = () => (
           </li>
         </UnorderedList>
       </Navigation>
-        <Switch>
-          <Route path="/zadania">
-            <TasksPage />
-          </Route>
-          <Route path="/autor">
-            <Author />
-          </Route>
-        </Switch>
+      <Switch>
+        <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/zadania">
+          <TasksPage />
+        </Route>
+        <Route path="/autor">
+          <Author />
+        </Route>
+      </Switch>
     </BrowserRouter>
   </>
 );
