b6434583cafe181eec91a087a23a8c80ed25423b
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-16 14:20:46 +0100

    Navigation Bar styled

--- src/styled.js
+++ src/styled.js
@@ -2,16 +2,24 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const Navigation = styled.nav`
-  background-color: ${({theme}) => theme.color.teal};
+  background-color: ${({ theme }) => theme.color.teal};
+  width: 100%;
+  height: 50px;
 `;
 
 export const UnorderedList = styled.ul`
   list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
 `;
 
 export const StyledNavLink = styled(NavLink)`
+  color: ${({ theme }) => theme.color.white};
   text-decoration: none;
-  color: ${({theme}) => theme.color.white};
+  display: block;
+  padding: 15px 10px; 
 
   &.active {
     font-weight: bold;
