940421df15e26c9a82677a7eeeee4e1eaf4c38b3
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-16 11:08:07 +0100

    App variable applied

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from "react";
 import Tasks from "./features/tasks/Tasks";
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
-export default () => (
-  <BrowserRouter>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/zadania">Zadania</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path="/zadania">
-          <Tasks />
-        </Route>
-      </Switch>
-    </nav>
-  </BrowserRouter>
+const App = () => (
+  <>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/zadania">Zadania</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/zadania">
+            <Tasks />
+          </Route>
+        </Switch>
+      </nav>
+    </BrowserRouter>
+  </>
 );
+
+export default App;
