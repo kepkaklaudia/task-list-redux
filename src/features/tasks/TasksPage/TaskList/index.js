ff3856b89b0dbf0e3cbd3893d37bb8b823d9a75b
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-17 15:16:15 +0100

    szukaj changed to searchQueryParamName

--- src/features/tasks/TasksPage/TaskList/index.js
+++ src/features/tasks/TasksPage/TaskList/index.js
@@ -3,10 +3,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { toggleTaskDone, removeTask, selectHideDone, selectTasksByQuery } from "../../tasksSlice";
 import { StyledLink } from "./styled";
 import { useLocation } from "react-router-dom";
+import { searchQueryParamName } from "../Search/searchQueryParamName";
 
 const TaskList = () => {
   const location = useLocation();
-  const query = (new URLSearchParams(location.search)).get("szukaj")
+  const query = (new URLSearchParams(location.search)).get(searchQueryParamName)
 
   const tasks = useSelector(state => selectTasksByQuery(state, query));
   const hideDone = useSelector(selectHideDone);
