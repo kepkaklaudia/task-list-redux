722186b2dda0f661bd141b57a20cfb7f349c748b
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-17 14:34:36 +0100

    useLocation added

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,6 +1,17 @@
+import { useLocation } from "react-router-dom";
+import { Details } from "../../../../common/Section/styled";
+import { Input } from "./../../Input/styled";
+
 const Search = () => {
-  return (
-    <>nothing</>
+  const location = useLocation();
+  const query = (new URLSearchParams(location.search)).get("szukaj");
+
+  return (<>
+    <Details>
+      <Input
+      />
+    </Details>
+  </>
   );
 };
 
