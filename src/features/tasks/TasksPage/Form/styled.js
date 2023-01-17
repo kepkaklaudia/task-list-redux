9aed39bb01aa6d7c4dd4cb69a3bd7a405cf5a085
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-17 13:52:19 +0100

    Input moved to separeted component

--- src/features/tasks/TasksPage/Form/styled.js
+++ src/features/tasks/TasksPage/Form/styled.js
@@ -12,23 +12,6 @@ export const StyledForm = styled.form`
   }
 `;
 
-export const Input = styled.input`
-  border: solid 2px ${({theme}) => theme.color.gallery};
-  padding: 10px;
-
-  &:focus{
-    border: solid 2px ${({theme}) => theme.color.white};
-    outline: solid 1px ${({theme}) => theme.color.black};
-  }
-
-  @media screen and (max-width: ${({theme}) => theme.breakpoint.mobileMax}px) {
-    &:focus{
-      border: solid 2px ${({theme}) => theme.color.white};
-      outline: solid 1px ${({theme}) => theme.color.orange};
-    }
-  }
-`;
-
 export const Button = styled.button`
   color: ${({theme}) => theme.color.white};
   background-color: ${({theme}) => theme.color.teal};
