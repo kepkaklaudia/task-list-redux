2fcb2c63a318022611c68a9509b3add3f1c2968b
Klaudia Kepka <kepkaklaudia@gmail.com>
2023-01-18 20:16:33 +0100

    About author added

--- src/features/tasks/author/Author.js
+++ src/features/tasks/author/Author.js
@@ -1,6 +1,7 @@
 import { Container } from "../../../common/Container/styled";
 import Header from "../../../common/Header";
 import Section from "../../../common/Section";
+import { Details } from "../../../common/Section/styled";
 
 const Author = () => (
   <>
@@ -9,10 +10,15 @@ const Author = () => (
         title="O autorze"
       />
       <Section
-        title="Klaudia Kępka"
-        body={<>tresc</>}
+        title="Klaudia Kępka 🤓"
+        body={<Details>
+          <p><strong>Cześć!</strong> 👆 Tak właśnie się nazywam i <strong>cieszę się, że tu jesteś!</strong>😄 </p>
+          <p>Ta aplikacja powstała w ramach kursu w Internetowej Szkole Programowania YouCode👩‍💻.</p>
+          <p>Programowanie od zawsze kojarzyło mi się z zajęciem interesującym, ale również pełnym wyzwań🦾.<br />
+            Na rok 2023 postawiłam sobie bardzo ambitne zadanie - <strong>zostanę programistką!</strong>🚀</p>
+        </Details>}
       />
-      </Container>
+    </Container>
   </>
 );
 
